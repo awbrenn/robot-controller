e819b702010d6dd1496427cf5002ad194245f88d
Stephen C Pickard <scpicka@imp21.cs.clemson.edu>
2015-04-18 14:35:38 -0400

    Changes in client

--- Client.cpp
+++ Client.cpp
@@ -15,7 +15,37 @@ client implementation for our program.
 #include "Header.h"
 using namespace std;
 
-int main (int argc, char *argv[]) {
-	
-	return 0;
+int main (int argc, char *argv[])
+{
+
+   int len = atoi(argv[1]);
+   int num = atoi(argv[2]);
+   float angle =  M_PI - ((M_PI * (num - 2)) / num);
+   
+   char turn[20];
+   sprintf(turn, "TURN %f", angle);
+   char move[20];
+   sprintf(move, "MOVE %d", len);
+      
+   printf("Len: %d, Num: %d, Angle: %f\n", len, num, angle);
+   
+   vector<string> v;
+ 
+   for (int i=0; i<num; i++)
+   {
+      v.push_back(move);
+      v.push_back("STOP");
+      v.push_back("GET IMAGE");
+      v.push_back("GET DGPS");
+      v.push_back("GET GPS");
+      v.push_back("GET LASERS");
+      v.push_back(turn);
+      v.push_back("STOP");
+   }
+   
+   vector<string>::iterator it;
+   for (it = v.begin(); it != v.end(); it++)
+      cout << *it << endl;
+   
+return 0;
 }

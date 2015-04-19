77e3299e91c6aef2d42eac59de050469b7e0a314
aki25 <akshitg@clemson.edu>
2015-04-19 01:08:09 -0400

    server sends commands and robot is responding successfully

--- Middleware.cpp
+++ Middleware.cpp
@@ -24,7 +24,7 @@ void getHTTPResponse(int);
 void buildRequestHeader();
 void sendRobotResponseToClient();
 void setupRobotConnection();
-
+void detectCommand(char *command);
 
 /* global variable declarations */
 unsigned short PORT = 8082; 		// default port value
@@ -32,6 +32,8 @@ string SERVER_NAME("");
 string FILE_PATH("/");
 string OUTPUT_FILENAME("");
 string HTTP_RESPONSE("");
+string STATE_ID("state?id=town2");
+string TWIST_ID("twist?id=town2&");
 string REQUEST_MESSAGE("GET ");
 int ROBOT_SOCKET;
 struct sockaddr_in ROBOT_ADDR; /* Local address */
@@ -41,13 +43,18 @@ struct sockaddr_in ROBOT_ADDR; /* Local address */
 int main (int argc, char *argv[]) {
     
     /* check for valid command line arguments */
-    if (argc != 2)
+    if (argc <2 || argc > 4) // 3rd arg = command. eg: MOVE ; 4th arg = value. eg: 5
     stateProperUsageAndDie(0);
 
 
     /* convert the URL to a string (to make parsing easier) */
    	string URL(argv[1]);
 	parseURL(URL); // then parse it
+
+	char incoming_request[20]; // tempory for now ; replace with recv's buffer when client is in action.
+	sprintf( incoming_request , "%s %s", argv[2],argv[3]); // request of form "GET GPS" or "MOVE 50"
+	/* detect the incomming command and build request accordingly ; currently takes command via cmd line arg */
+	detectCommand(incoming_request);
 
     /*build the header of the GET request*/
     buildRequestHeader();
@@ -136,6 +143,74 @@ void getHTTPResponse(int ROBOT_SOCKET) {
         HTTP_RESPONSE += httpRequestBuffer[i];
     }
     HTTP_RESPONSE += '\n';
+}
+
+void detectCommand(char *command)
+{
+	// image is not yet functional
+	// when satisfied , remove all the cout(s) to make the code more readable and concise.
+
+	char *temp;
+	char *value = 0;
+
+	if(strcmp(command , "GET IMAGE")==0)
+	{
+		PORT = 8081;
+		cout<<"request for image"<<endl;
+	}
+	else if(strcmp(command , "GET GPS")==0)
+	{
+		FILE_PATH = FILE_PATH + STATE_ID ;
+		printf("%s\n", FILE_PATH.c_str());
+		cout<<"request for gps"<<endl;
+	}
+	else if(strcmp(command , "GET DGPS")==0)
+	{
+		FILE_PATH = FILE_PATH + STATE_ID ;
+		printf("%s\n", FILE_PATH.c_str());
+		PORT = 8084;
+		cout<<"request for dgps"<<endl;
+	}
+	else if(strcmp(command , "GET LASERS")==0)
+	{
+		FILE_PATH = FILE_PATH + STATE_ID ;
+		printf("%s\n", FILE_PATH.c_str());
+		PORT = 8083;
+		cout<<"request for lasers"<<endl;
+	}
+	else if(strstr(command , "MOVE")!=NULL)
+	{
+		printf("~~ we like to move it move it ~~\n");
+		temp = strrchr(command , ' ');
+		temp++;
+		value= temp;
+		FILE_PATH = FILE_PATH + TWIST_ID + "lx=" + value ;
+		printf("%s\n", FILE_PATH.c_str());
+		cout<<"request for movement"<<endl;
+		cout<<"move value is : "<<value<<endl;
+	}
+	else if(strstr(command , "TURN")!=NULL)
+	{
+		temp = strrchr(command , ' ');
+		temp++;
+		value = temp;
+		FILE_PATH = FILE_PATH + TWIST_ID + "az=" + value ;
+		printf("%s\n", FILE_PATH.c_str());
+		cout<<"request for turn"<<endl;
+		cout<<"turn value is : "<<value<<endl;
+	}
+	else if(strstr(command , "STOP")!=NULL)
+	{
+		printf(" sthaappp! \n");
+		FILE_PATH = FILE_PATH + TWIST_ID + "lx=0";
+		printf("%s\n", FILE_PATH.c_str());
+		cout<<"request for stop"<<endl;
+		cout<<"move value is : "<<value<<endl;
+	}
+
+	else
+		cout<<"unknown command"<<endl;
+
 }
 
 /*********************************

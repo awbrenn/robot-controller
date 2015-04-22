7215ce0130a529aa00022b56242a4d66be167868
scpicka <scpicka@g.clemson.edu>
2015-04-22 12:05:07 -0400

    Major changes 2

--- Client.cpp
+++ Client.cpp
@@ -13,25 +13,32 @@ client implementation for our program.
 *********************************************************/
 
 #include "Header.h"
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <cstdlib>
-#include <vector>
-#include <stdlib.h>
+//#include <string>
+//#include <cstdlib>
 #include <math.h>
-#include <time.h>
-#include <sys/time.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <signal.h>
-#include <ctime>
-#include <time.h>
+//#include <ctime>
 
 using namespace std;
-
+//Functions
 void catchAlarm(int);
+void createVector(double angle, int len, int num);
+void initalizeMiddleware();
+string convertHeaderInfoToString(uint32_t header_info);
+vector<string> addHeaderToResponseToClient(vector<string> commands);
+
+//Globals
+vector<string> v;
+int sock;
+struct sockaddr_in servAddr;
+struct sockaddr_in clntAddr;
+struct hostent *thehost;
+unsigned int clntAddrLen;
+int port;
+char *ip;
+char sendBuffer[1000];
+char recvBuffer[1000];
+uint32_t REQUEST_ID = 546; 
+string ROBOT_ID = "town2";
 
 int main (int argc, char *argv[])
 {
@@ -39,66 +46,35 @@ int main (int argc, char *argv[])
    int len = atoi(argv[1]);
    int num = atoi(argv[2]);
    float angle =  M_PI - ((M_PI * (num - 2)) / num);
-   int port = atoi(argv[3]);
-   char *ip = argv[4];
-   unsigned int TIMEOUT_SECONDS = 5;
-   struct sigaction timeoutAction;
-   char sendBuffer[1000];
-   char recvBuffer[1000];
+   port = atoi(argv[3]);
+   ip = argv[4];
    float speed = 0.2;
    
-   int sock;
-   struct sockaddr_in servAddr;
-   struct sockaddr_in clntAddr;
-   struct hostent *thehost;
-   unsigned int clntAddrLen;
-   int recvMsgSize;
-   int sendMsgSize;
-   
+   unsigned int TIMEOUT_SECONDS = 5;
+   struct sigaction timeoutAction;
    clock_t startTime;
    double secondsPassed;
-
-   char turn[20];
-   sprintf(turn, "TURN %f", angle);
-   char move[20];
-   sprintf(move, "MOVE %d", len);
-      
+ 
+   int recvMsgSize;
+  
    printf("Len: %d, Num: %d, Angle: %f\n", len, num, angle);
    cout << "Port: " << port << " IP: " << ip << endl;
-   vector<string> v;
- 
-   for (int i=0; i<num; i++)
-   {
-      v.push_back(move);
-      v.push_back("STOP");
-      v.push_back("GET IMAGE");
-      v.push_back("GET DGPS");
-      v.push_back("GET GPS");
-      v.push_back("GET LASERS");
-      v.push_back(turn);
-      v.push_back("STOP");
-   }
+   
+   //Call function to populate the vector
+   createVector(angle, len, num);
+
+
+   
+   //Call function to start connection
+   initalizeMiddleware();   
+   
+   //add header
+   v = addHeaderToResponseToClient(v);
    
    vector<string>::iterator it;
    for (it = v.begin(); it != v.end(); it++)
       cout << *it << endl;
-      
-   // Create socket for sending/receiving datagrams 
-   if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
-      cout << "socket() failed" << endl;
-
-   // Construct local address structure 
-   memset(&servAddr, 0, sizeof(servAddr));   // Zero out structure 
-   servAddr.sin_family = AF_INET;                  // Internet address family 
-   servAddr.sin_addr.s_addr = htonl(INADDR_ANY);   // Any incoming interface 
-   servAddr.sin_port = htons(port);       // Local port    
    
-   // If user gave a dotted decimal address, we need to resolve it  
-   if (servAddr.sin_addr.s_addr == -1) {
-      thehost = gethostbyname(ip);
-      servAddr.sin_addr.s_addr = *((unsigned long *) thehost->h_addr_list[0]);
-   }
-	
    //Run loop until all of v has been sent 
 //   for (it = v.begin(); it != v.end(); it++)
 //   {
@@ -110,10 +86,9 @@ int main (int argc, char *argv[])
    printf("enter guess: ");
    gets(sendBuffer);
    cout << "Guess is: " << sendBuffer << endl;
-   
+    
    //Send command
-	sendMsgSize = strlen(sendBuffer);
-	if (sendto(sock, sendBuffer, sendMsgSize, 0, (struct sockaddr *) &servAddr, sizeof(servAddr)) != sendMsgSize)
+	if (sendto(sock, v.at(1).c_str(), v.at(1).length(), 0, (struct sockaddr *) &servAddr, sizeof(servAddr)) != v.at(1).length())
   		cout << "Error on sendto" << endl; 
    
    //start timer
@@ -140,11 +115,12 @@ int main (int argc, char *argv[])
       {
          double timePassed1 = difftime(time(0), startTime);
          cout << "Timeout occured. Program Ending. Time passed is " << timePassed1 << endl;
+         exit(0);
       }
       
    //Sleep based on timer if move or send
    double timePassed2 = difftime(time(0), startTime);
-   sleep((len/speed) - timePassed); //Need to check math
+   sleep((len/speed) - timePassed2); //Need to check math
 //   }
    
    
@@ -152,3 +128,95 @@ return 0;
 }
 
 void catchAlarm(int ignored) { return; }
+
+void createVector(double angle, int len, int num)
+{
+   char turnFirst[20];
+   sprintf(turnFirst, "TURN %f", angle);
+   char moveFirst[20];
+   sprintf(moveFirst, "MOVE %d", len);
+ 
+   for (int i=0; i<num; i++)
+   {
+      v.push_back(moveFirst);
+      v.push_back("STOP");
+      v.push_back("GET IMAGE");
+      v.push_back("GET DGPS");
+      v.push_back("GET GPS");
+      v.push_back("GET LASERS");
+      v.push_back(turnFirst);
+      v.push_back("STOP");
+   }
+
+   char turnSecond[20];
+   sprintf(turnSecond, "TURN -%f", angle);
+   char moveSecond[20];
+   sprintf(moveSecond, "MOVE %d", len);
+   
+   for (int j=0; j<num-1; j++)
+   {
+      v.push_back(moveSecond);
+      v.push_back("STOP");
+      v.push_back("GET IMAGE");
+      v.push_back("GET DGPS");
+      v.push_back("GET GPS");
+      v.push_back("GET LASERS");
+      v.push_back(turnSecond);
+      v.push_back("STOP");
+   }
+}
+
+void initalizeMiddleware()
+{
+// Create socket for sending/receiving datagrams 
+   if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+      cout << "socket() failed" << endl;
+
+   // Construct local address structure 
+   memset(&servAddr, 0, sizeof(servAddr));   // Zero out structure 
+   servAddr.sin_family = AF_INET;                  // Internet address family 
+   servAddr.sin_addr.s_addr = htonl(INADDR_ANY);   // Any incoming interface 
+   servAddr.sin_port = htons(port);       // Local port    
+   
+   // If user gave a dotted decimal address, we need to resolve it  
+   if (servAddr.sin_addr.s_addr == -1) {
+      thehost = gethostbyname(ip);
+      servAddr.sin_addr.s_addr = *((unsigned long *) thehost->h_addr_list[0]);
+   }
+}
+
+string convertHeaderInfoToString(uint32_t header_info) 
+{
+    string header_info_string;
+    char uint4_chunk;
+    int size_of_uint32_t = 4;
+    int bits_in_a_byte = 8;
+
+    cout << "HEADER INFO:" << header_info << endl;
+
+    for (int i = 0; i < size_of_uint32_t; ++i) {
+        uint4_chunk = (char) ((header_info << (i * bits_in_a_byte)) >> (3 * bits_in_a_byte));
+        cout << "step " << i << ": " << header_info << ", " << (uint32_t) uint4_chunk << endl;
+        header_info_string.push_back(uint4_chunk);
+    }
+
+    return header_info_string;
+}
+
+vector<string> addHeaderToResponseToClient(vector<string> commands) 
+{
+    uint32_t number_of_commands = commands.size();
+    string body;
+    int i;
+
+    /* add header to each of the fragmented responses */
+    for (i = 0; i < (int) number_of_commands; i++) {
+        body = commands[i];
+        body = convertHeaderInfoToString(REQUEST_ID)
+             + ROBOT_ID
+             + body;
+        commands[i] = body;
+    }
+
+    return commands;
+}

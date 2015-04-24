c38605016f24a74310bb8af9fc1465b95bde6f60
scpicka <scpicka@g.clemson.edu>
2015-04-23 21:18:11 -0400

    Adding sleep

--- Client.cpp
+++ Client.cpp
@@ -28,7 +28,7 @@ vector<string> addHeaderToCommands(vector<string> commands);
 void recieveAckFromMiddleware();
 
 //Globals
-vector<string> v;
+vector<string> shape1, shape2;
 int sock;
 struct sockaddr_in servAddr;
 struct sockaddr_in clntAddr;
@@ -41,6 +41,7 @@ uint32_t REQUEST_ID = 546;
 string ROBOT_ID = "town2";
 float angleFirst, angleSecond;
 
+
 int main (int argc, char *argv[])
 {
 
@@ -48,12 +49,19 @@ int main (int argc, char *argv[])
    int num = atoi(argv[4]);
    port = atoi(argv[2]);
    ip = argv[1];
-   float speed = 0.2;
+   double moveSpeed = 0.2;
+   double turnSpeed = 0.2;
+   double moveSleep, turnSleep;
+   size_t found;
    
    unsigned int TIMEOUT_SECONDS = 5;
    struct sigaction timeoutAction;
   
    cout << "Port: " << port << " IP: " << ip << endl;
+   
+   //Open file for output
+   FILE *output;
+   output = fopen("output.txt", "w");
    
    //Set timeout
    timeoutAction.sa_handler = catchAlarm;
@@ -70,7 +78,8 @@ int main (int argc, char *argv[])
    initalizeMiddleware();   
    
    //Call function to add header to vector components
-   //v = addHeaderToCommands(v);
+   //shape1 = addHeaderToCommands(shape1);
+   //shape2 = addHeaderToCommands(shape2);
    
    printf("Len: %d, Num: %d, AngleFirst: %f, AngleSecond: %f\n", len, 
       num, angleFirst, angleSecond);
@@ -79,29 +88,84 @@ int main (int argc, char *argv[])
    vector<string>::iterator it;
    // for (it = v.begin(); it != v.end(); it++)
    //    cout << *it << endl;
-   
-   
+
    int i = 0;
-   //Run loop until all of v has been sent 
-   for (it = v.begin(); it != v.end(); it++)
+   //Run loop until all of shape1 has been sent 
+   for (it = shape1.begin(); it != shape1.end(); it++)
    {
       memset (recvBuffer, 0, sizeof(recvBuffer));
       clntAddrLen = sizeof(clntAddr);
        
       //Send command
-      if (sendto(sock, v.at(i).c_str(), v.at(i).length(), 0, 
-         (struct sockaddr *) &servAddr, sizeof(servAddr)) != v.at(i).length())
+      if (sendto(sock, shape1.at(i).c_str(), shape1.at(i).length(), 0, 
+         (struct sockaddr *) &servAddr, sizeof(servAddr)) != shape1.at(i).length())
+	      cout << "Error on sendto" << endl; 
+
+      alarm(TIMEOUT_SECONDS);
+
+      //Recv ack
+      recieveAckFromMiddleware();
+
+      //Shape1 sleep for move
+      found = shape1.at(i).find("MOVE");
+      if (found != std::string::npos)
+      { 
+         cout << "Sleeping for shape1 move" << endl;
+         moveSleep = len / moveSpeed * 1000000;
+         usleep(moveSleep);
+      }
+      
+      //Shape1 sleep for turn
+      found = shape1.at(i).find("TURN");
+      if (found != std::string::npos)
+      {
+         cout << "Sleeping for shape1 turn" << endl;
+         turnSleep = angleFirst / turnSpeed * 1000000;
+         usleep(turnSleep);
+      }
+      
+      i++;
+   }
+   
+   i = 0;
+   //Run loop until all of shape2 has been sent 
+   for (it = shape2.begin(); it != shape2.end(); it++)
+   {
+      memset (recvBuffer, 0, sizeof(recvBuffer));
+      clntAddrLen = sizeof(clntAddr);
+       
+      //Send command
+      if (sendto(sock, shape2.at(i).c_str(), shape2.at(i).length(), 0, 
+         (struct sockaddr *) &servAddr, sizeof(servAddr)) != shape2.at(i).length())
 	      cout << "Error on sendto" << endl; 
          
       alarm(TIMEOUT_SECONDS);
 
       //Recv ack
       recieveAckFromMiddleware();
+      
+      //Shape2 sleep for move
+      found = shape2.at(i).find("MOVE");
+      if (found != std::string::npos)
+      { 
+         cout << "Sleeping for shape2 move" << endl;
+         moveSleep = len / moveSpeed * 1000000;
+         usleep(moveSleep);
+      }
+      //Shape2 sleep for turn
+      found = shape2.at(i).find("TURN");
+      if (found != std::string::npos)
+      {
+         cout << "Sleeping for shape2 turn" << endl;
+         turnSleep = angleSecond / turnSpeed * 1000000;
+         usleep(turnSleep);
+      }
+      
       i++;
    }
    
-   
-return 0;
+   fclose(output);
+   return 0;
 }
 
 void catchAlarm(int ignored) { return; }
@@ -117,14 +181,14 @@ void createVector(int len, int num)
  
    for (int i=0; i<num; i++)
    {
-      v.push_back(moveFirst);
-      v.push_back("STOP");
-      //v.push_back("GET IMAGE");
-      v.push_back("GET DGPS");
-      v.push_back("GET GPS");
-      v.push_back("GET LASERS");
-      v.push_back(turnFirst);
-      v.push_back("STOP");
+      shape1.push_back(moveFirst);
+      shape1.push_back("STOP");
+      //shape1.push_back("GET IMAGE");
+      shape1.push_back("GET DGPS");
+      shape1.push_back("GET GPS");
+      shape1.push_back("GET LASERS");
+      shape1.push_back(turnFirst);
+      shape1.push_back("STOP");
    }
 
    char turnSecond[20];
@@ -134,14 +198,14 @@ void createVector(int len, int num)
    
    for (int j=0; j<num-1; j++)
    {
-      v.push_back(moveSecond);
-      v.push_back("STOP");
-      //v.push_back("GET IMAGE");
-      v.push_back("GET DGPS");
-      v.push_back("GET GPS");
-      v.push_back("GET LASERS");
-      v.push_back(turnSecond);
-      v.push_back("STOP");
+      shape2.push_back(moveSecond);
+      shape2.push_back("STOP");
+      //shape2.push_back("GET IMAGE");
+      shape2.push_back("GET DGPS");
+      shape2.push_back("GET GPS");
+      shape2.push_back("GET LASERS");
+      shape2.push_back(turnSecond);
+      shape2.push_back("STOP");
    }
 }
 
